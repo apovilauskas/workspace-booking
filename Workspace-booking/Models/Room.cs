b828a7d7438df91856e41a08986ce868cd8b1bb2
Augustas P <augustaspovil@gmail.com>
2026-06-09 15:47:46 +0300

    record set up

---
+++ Workspace-booking/Models/Room.cs
@@ -0,0 +1,5 @@
+﻿namespace Workspace_booking.Models;
+
+public record Room(int Id, string Name, int Capacity, string BuildingFloor);
+
+public record Booking(int Id, int RoomId, string RoomName, string EmployeeName, DateTime Date, int hours);
---
+++ Workspace-booking/Services/IBookingService.cs
@@ -0,0 +1,11 @@
+﻿using Workspace_booking.Models;
+
+namespace Workspace_booking.Services;
+
+public interface IBookingService
+{
+    Task<List<Room>> GetRoomsAsync();
+    Task<List<Booking>> GetBookingAsync(); 
+    Task AddBookingAsync(Booking booking);
+
+}
