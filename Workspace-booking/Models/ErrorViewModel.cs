18ddba1ebca8481bec11ad7b4a442b61ae0d3bba
Augustas P <augustaspovil@gmail.com>
2026-06-09 11:05:25 +0300

    initial commit

---
+++ Workspace-booking/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Workspace_booking.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
